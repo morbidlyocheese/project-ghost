e72b285bc45864c4bab0c8814363e10e353c0866
Dandelion Jacobson <morbidlyocheese@gmail.com>
2024-12-19 03:33:50 -0800

    🔥 third person camera added

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -1,14 +1,25 @@
 extends CharacterBody3D
 
+@onready var spring_arm_pivot = $SpringArmPivot
+@onready var spring_arm = $SpringArmPivot/SpringArm3D
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # gravity controlled in project settings
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+	
+	if event is InputEventMouseMotion:
+		spring_arm_pivot.rotate_y(-event.relative.x * .005)
+		spring_arm.rotate_x(-event.relative.y * .005)
+		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
